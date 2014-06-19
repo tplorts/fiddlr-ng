afa1e73c0bf5ff7bbf8ec531616bfc569b80b9f9
Ted Lorts <tplorts@gmail.com>
2014-06-19 12:32:33 -0400

    generalized the angular include markup

---
+++ home/templatetags/angular.py
@@ -0,0 +1,14 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('tags/angular.html')
+def angular( *args, **kwargs ):
+    if 'version' not in kwargs:
+        kwargs['version'] = '1.3.0-beta.10'
+    return kwargs
+
+@register.inclusion_tag('tags/angular-app.html')
+def angular_app( app_directory ):
+    return {'app_directory': app_directory}
