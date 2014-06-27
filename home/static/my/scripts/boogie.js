a134955e02ff9e5f18f5e21f06ebc247832b94bb
Ted Lorts <tplorts@gmail.com>
2014-06-26 23:30:48 -0400

    some disco fixes

--- home/static/my/scripts/boogie.js
+++ home/static/my/scripts/boogie.js
@@ -3,23 +3,38 @@ var boogieb;
 var boo;
 var dball;
 var backdrop;
+var isDiscoing = false;
+
 $(window).on('load', function() {
     boogieb = $('.boogie-button');
     boo = document.getElementById('boogieoogieoogie');
     dball = $('#discoball');
     backdrop = $('.disco-backdrop');
     boogieb.click( function() {
-        boo.play();
+        if( !isDiscoing ) {
+            boo.play();
+        }
+        isDiscoing = true;
         lowerDiscoball();
+    });
+    $('#boogieoogieoogie').on('ended', function() {
+        raiseDiscoball();
+        isDiscoing = false;
     });
 });
 
 function lowerDiscoball() {
+    backdrop.css('transition-delay', '0s');
+    dball.css('transition-delay', '3s');
+
     backdrop.addClass('black-backdrop');
     dball.addClass('dothedisco');
 }
 
 function raiseDiscoball() {
+    backdrop.css('transition-delay', '6s');
+    dball.css('transition-delay', '0s');
+
     backdrop.removeClass('black-backdrop');
     dball.removeClass('dothedisco');
 }
