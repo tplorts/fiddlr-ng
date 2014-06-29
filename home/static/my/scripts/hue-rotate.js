ed2a39cbb694ca545e6213e2c15bdb1593afab64
Ted Lorts <tplorts@gmail.com>
2014-06-29 16:08:14 -0400

    well whatever, mozilla still hasn't implemented filters

--- home/static/my/scripts/hue-rotate.js
+++ home/static/my/scripts/hue-rotate.js
@@ -1,8 +1,13 @@
 var huerotation = 0;
 function hueRotateStep() {
-	var spinee = $(".hue-rotate");
-	spinee.css("-webkit-filter", "hue-rotate(" + huerotation + "deg)");
+	var spinee = $('.hue-rotate');
 	huerotation = (huerotation + 3) % 360;
+    var filter = 'hue-rotate(' + huerotation + 'deg)';
+	spinee.css({
+        '-webkit-filter': filter,
+        '-moz-filter': filter,
+        'filter': filter
+    });
 };
 
 setInterval(hueRotateStep, 100);
