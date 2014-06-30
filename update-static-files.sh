8090001d8d5903d3e5709999f68f70143eff128f
Ted Lorts <tplorts@gmail.com>
2014-06-30 19:32:08 -0400

    FINALLY put the admin static files into s3

--- update-static-files.sh
+++ update-static-files.sh
@@ -3,6 +3,10 @@ cd home/static/my/styles
 lessc -x styles.less styles.compiled.css
 
 
-echo " == Beginning upload of new static files"
+echo " == my static files"
 cd ../..
 aws s3 sync . s3://fiddlr-static   --exclude "node_modules/*"    --acl public-read
+
+echo " == admin static files"
+cd ../..
+aws s3 sync venv/lib/python2.7/site-packages/django/contrib/admin/static/ s3://fiddlr-static --acl public-read
