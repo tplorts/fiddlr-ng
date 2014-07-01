042437edb4531193257b441e45e516e51892f587
Ted Lorts <tplorts@gmail.com>
2014-06-30 20:56:23 -0400

    Hey Bobby, why do you like Soul Food?  Because it makes me HAAAAPPY

---
+++ home/static/my/scripts/soulfood.js
@@ -0,0 +1,12 @@
+
+var soulfood;
+$(window).on('load', function() {
+    soulfood = document.getElementById('soulfoodhaaappy');
+    $('#signout-button').click( function() {
+        soulfood.play();
+        return false;
+    });
+    $('#soulfoodhaaappy').on('ended', function() {
+        document.getElementById('signout-form').submit();
+    });
+});
