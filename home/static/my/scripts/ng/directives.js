05f8f9a7b11abb7bb2146d55b46a2abec3153793
Ted Lorts <tplorts@gmail.com>
2014-07-02 14:27:06 -0400

    for now, not going to make custom tooltip directive

--- home/static/my/scripts/ng/directives.js
+++ home/static/my/scripts/ng/directives.js
@@ -4,14 +4,3 @@
 var dmod = angular.module('fiddlrApp.directives', []);
 
 
-
-dmod.directive('fiddlrTooltipIfOverflow', function() {
-    return {
-        restrict: 'A',
-        link: function(scope, element) {
-            var r = element[0].offsetWidth - element[0].children[0].offsetWidth;
-            if( r < 0 )
-                console.log('too small: '+r);
-        }
-    };
-});
