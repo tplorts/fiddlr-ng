51529cdbd1ffc73b62e2e887aeb0eb29b1f7a31a
Ted Lorts <tplorts@gmail.com>
2014-07-07 10:14:09 -0400

    moved the profile templates

--- home/views.py
+++ home/views.py
@@ -240,17 +240,17 @@ def connect(q):
 
 
 def artist_page(q, artist_id):
-    return renderPage(q, 'artist/page', {
+    return renderPage(q, 'profiles/artist-profile', {
         'artist': Artist.objects.get(pk=artist_id),
     })
 
 def venue_page(q, venue_id):
-    return renderPage(q, 'venue/page', {
+    return renderPage(q, 'profiles/venue-profile', {
         'venue': Venue.objects.get(pk=venue_id),
     })
 
 def event_page(q, event_id):
-    return renderPage(q, 'event/page', {
+    return renderPage(q, 'profiles/event-profile', {
         'event': Event.objects.get(pk=event_id),
     })
 
